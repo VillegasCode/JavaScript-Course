091e077036272c7d6d6417200778c0c2e89ecbf9
Carlos Villegas <carlos.villegasch@gmail.com>
2022-10-25 17:38:29 -0500

    Operadores Aritméticos en JS

---
+++ 04-Operadores-en-JS/OperadoresJS.js
@@ -0,0 +1,43 @@
+//OPERADORES ARITMÉTICOS
+let operando1 = 3;
+let operando2 = 2;
+let resultado;
+
+//SUMA o ADICIÓN
+resultado = operando1 + operando2;
+console.log(resultado);
+
+//RESTA o SUSTRACCIÓN
+resultado = operando1 - operando2;
+console.log(resultado);
+
+//MULTIPLICACIÓN
+resultado = operando1 * operando2;
+console.log(resultado);
+
+//DIVISIÓN
+resultado = operando1 / operando2;
+console.log(resultado);
+
+//RESTO o MÓDULO
+resultado = operando1 % operando2;
+console.log(resultado);
+
+//EXPONENCIACIÓN
+resultado = operando1 ** operando2;
+console.log(resultado);
+
+//INCREMENTO ANTICIPADO DE OPERANDO + 1 PARA PASARLE EL VALOR AL RESULTADO
+resultado = ++operando1;
+console.log(resultado);
+
+//INCREMENTO DESPUÉS DE PASARLE EL VALOR A RESULTADO
+operando1 = 3; //Le asignamos el valor 3 solo para hacer este ejemplo
+resultado = operando1++;
+console.log(resultado);
+console.log(operando1);
+
+//DECREMENTO
+resultado = operando2--;
+console.log(resultado);
+console.log(operando2);
