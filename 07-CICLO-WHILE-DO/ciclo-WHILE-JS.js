d448e2692d346e22afe9a800bb6c1c42d395f3d3
Carlos Villegas <carlos.villegasch@gmail.com>
2022-10-26 14:11:49 -0500

    Uso del Ciclo WHILE y Ciclo DO WHILE

---
+++ 07-CICLO-WHILE-DO/ciclo-WHILE-JS.js
@@ -0,0 +1,7 @@
+//CICLO WHILE
+
+let cont = 1;
+while (cont <= 100) {
+    document.write(cont + " ");
+    cont++;
+}
---
+++ 07-CICLO-WHILE-DO/ciclo-do-while-JS.js
@@ -0,0 +1,6 @@
+//CICLO DO WHILE
+let cont = 1;
+do {
+    document.write(cont + " ");
+    cont++;
+} while (cont <= 0);
