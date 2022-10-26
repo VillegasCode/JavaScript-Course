e676189cd037903474f3eab9bbfaf4601db43ce3
Carlos Villegas <carlos.villegasch@gmail.com>
2022-10-25 19:07:50 -0500

    04. Operadores Ternarios

---
+++ 04-Operadores-en-JS/04. Operadores-Ternario.js
@@ -0,0 +1,9 @@
+//04. OPERADORES TERNARIO
+
+resultado = (30 === 30) ? 'SI es igual': 'NO es igual';
+
+console.log(resultado);
+
+
+resultado = (30 > 60) ? 'SI es mayor': 'NO es mayor';
+console.log(resultado);
