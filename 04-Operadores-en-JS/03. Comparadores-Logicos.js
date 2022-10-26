6de9e0877981b640e66648fbdab51c98a796b95a
Carlos Villegas <carlos.villegasch@gmail.com>
2022-10-25 19:03:33 -0500

    03. Comparadores Logicos

---
+++ 04-Operadores-en-JS/03. Comparadores-Logicos.js
@@ -0,0 +1,14 @@
+//03. COMPARADORES LÓGICOS
+
+let operando1 = true;
+let operando2 = false;
+let operando3 = true;
+
+//OPERADOR AND respresentado con &&
+console.log(operando1 && operando2);
+console.log(operando1 && operando3);
+
+//OPERADOR OR representado con ||
+operando3 = false;
+console.log(operando1 || operando2);
+console.log(operando2 || operando3);
