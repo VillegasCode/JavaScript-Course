cbf05bfbfd58abc41c8e6b940118a77a5d77e06d
Carlos Villegas <carlos.villegasch@gmail.com>
2022-10-29 20:27:38 -0500

    Clase sobre Metodos-Estaticos

--- 14-Atributos-Metodos-Estaticos/Atributos-estaticos.js
+++ 14-Atributos-Metodos-Estaticos/Atributos-estaticos.js
@@ -16,7 +16,7 @@ class Mascota {
     }
 
     static saludo() {
-        return 'mover cola';
+        return 'mover la cola';
     }
 }
 
@@ -33,3 +33,29 @@ console.log(Mascota.contadorMascota);
 console.log(mascota1.numeroCreacion);
 console.log(mascota2.numeroCreacion);
 console.log(mascota3.numeroCreacion);
+
+
+//MÉTODOS ESTÁTICOS
+console.log(Mascota.saludo());
+
+//SUBCLASE HIJA DE LA CLASE PADRE MASCOTA
+class Perro extends Mascota {
+    //A los atributos de la clase padre le agregamos un atributo más o los que queramos
+    constructor(nombre, edad, raza) {
+        //Accedemos a los atributos de la clase padre con la palabra reservada super
+        super(nombre, edad);
+        //Agregamos el atributo adicional
+        this._raza = raza;
+    }
+}
+
+console.log(Perro.cola);
+console.log(Perro.saludo());
+console.log(Perro.contadorMascota);
+
+let perro1 = new Perro('Rufus', 10, 'Dalmata');
+
+console.log(Perro.contadorMascota);
+console.log(perro1._nombre);
+console.log(perro1._edad);
+console.log(perro1._raza);
