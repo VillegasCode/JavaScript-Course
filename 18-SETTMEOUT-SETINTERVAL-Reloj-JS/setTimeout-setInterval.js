1801beffd9dffac2c952b9b86acdfcd8966aeb54
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-01 23:58:23 -0500

    Actualización del saludo

---
+++ 18-SETTMEOUT-SETINTERVAL-Reloj-JS/setTimeout-setInterval.js
@@ -0,0 +1,7 @@
+// setTimeout - Ejecuta una funcion DESPUÉS DE CIERTO TIEMPO
+function saludo(){
+    console.log(`Hola, Buenos días amigos`);
+}
+
+//Ejecutar la función y mostrar en consola después de 5 segundos
+setTimeout(saludo, 5000);
