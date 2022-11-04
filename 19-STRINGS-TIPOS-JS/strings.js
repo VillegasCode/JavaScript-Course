5831af9c8d5b2f630ac07f332bd847164607c483
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-03 21:45:58 -0500

    Tipos de strings y manejo

--- 19-STRINGS-TIPOS-JS/strings.js
+++ 19-STRINGS-TIPOS-JS/strings.js
@@ -35,3 +35,34 @@ console.log(saludo[0]);
 console.log(saludo.toUpperCase());
 console.log(saludo.toLowerCase());
 
+//SUBCADENAS - Con indexOf te muestra la posición exacta del string
+let frase = 'HTML es un lenguaje de marcado y Javascript es un lenguaje de programación';
+console.log(frase.indexOf('HTML'));
+console.log(frase.indexOf('lenguaje'));
+//Te muestra la posición de la palabra si es que existe a partir de la posición 6
+console.log(frase.indexOf('es', 6));
+
+//Cuando el resultado es -1 significa que no se encontró dicha palabara en la subcadena
+console.log(frase.indexOf('css'));
+
+//lastIndexOf empieza a contar de atrás hacia delante
+console.log(frase.lastIndexOf('es'));
+
+//includes para obtener un valor TRUE or FALSE
+console.log(frase.includes('lenguaje'));
+
+
+//SUBSTRINGS
+let lenguaje = 'Javascript';
+console.log(lenguaje.substring(2,4));
+
+console.log(lenguaje.substr(2,4));
+
+
+//MÉTODO TRIM  y REPEAT para eliminar espacios en blanco
+let lenguaje1 = ' Javascript y CSS ';
+lenguaje1 = lenguaje1.trim();
+console.log(lenguaje1);
+
+//REPEAT
+console.log(lenguaje1.repeat(5));
