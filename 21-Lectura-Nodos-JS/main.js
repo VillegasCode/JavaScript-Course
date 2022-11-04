d917814625bd579ce9fd7929bd3eff0e946ba058
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-04 12:20:20 -0500

    Convertir NodeList en Array

--- 21-Lectura-Nodos-JS/main.js
+++ 21-Lectura-Nodos-JS/main.js
@@ -10,9 +10,28 @@ elementoPorID.innerHTML = 'Suscríbete';
 //Por nombre de la clase
 //let elementosPorClase = document.getElementsByClassName('parrafos');
 //Esto trae el primer elemento de la clase cuando hay varias clases
-let elementosPorClase = document.querySelector('.parrafos');
-console.log(elementosPorClase);
+// let elementosPorClase = document.querySelector('.parrafos');
+// console.log(elementosPorClase);
 
 //querySelectorAll para que nos traiga un listado (NodeList)
 elementosPorClase = document.querySelectorAll('.parrafos');
 console.log(elementosPorClase);
+//Usar un operador de propagación (agregar 3 puntos suspensivos antes del NODELIST) para convertir nuestro node list en un arreglo
+const arreglo = [...elementosPorClase];
+console.log(arreglo);
+arreglo[1].innerHTML = 'a VillegasCode';
+
+//Por Etiqueta
+//let elementosPorEtiquetas = documen.getElementsByTagName('p');
+let elementosPorEtiquetas = document.querySelectorAll('p');
+elementosPorEtiquetas[2].innerHTML = 'ahora!';
+
+
+let elemento1H2 = document.getElementsByTagName('h2');
+elemento1H2[0].innerHTML = 'PRUEBA';
+
+//Cambiando el contenido de la segunda etiqueta H2 del documento index.html
+let elementoH2 = document.querySelectorAll('h2');
+//Indicamos la posición de la etiqueta que queremos cambiar
+elementoH2[1].innerHTML = 'Carlos';
+elementoH2[0].innerHTML = 'querySelector' + '<br>' + '& querySelectorAll';
