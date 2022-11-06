067b5ec6a2e77768a1c8276639686bac495c6f1c
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-05 20:22:19 -0500

    addEventListener con click derecho con contextmenu

--- 25-EVENTLISTENERS/main.js
+++ 25-EVENTLISTENERS/main.js
@@ -1,3 +1,7 @@
 saludoBtn.addEventListener('click', ()=>{
     console.log('Hola desde funcion flecha');
 });
+
+saludoBtn1.addEventListener('contextmenu', ()=>{
+    console.log('Haz hecho clic derecho');
+});
