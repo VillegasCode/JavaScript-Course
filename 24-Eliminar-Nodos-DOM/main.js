95a5c71484b54d0b0b97feae644420fbc889d5bc
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-05 19:51:43 -0500

    Eliminando nodos con Remove y ReplaceChild

--- 24-Eliminar-Nodos-DOM/main.js
+++ 24-Eliminar-Nodos-DOM/main.js
@@ -10,9 +10,19 @@ parrafoCuatro.innerHTML = 'Párrafo 4';
 
 //ELIMINAR NODO
 const parrafoUno = document.querySelector('#parrafo1');
-
 //RemoveChild
 elementoPadre.removeChild(parrafoUno);
 
 
-//ELIMINAR PÁRRAFO 2
+
+//ELIMINAR NODE PÁRRAFO 2 - Forma 2
+const parrafoDos = document.querySelector('#parrafo2');
+//REMOVE
+parrafoDos.remove();
+
+
+
+//ELIMINAR NODE PÁRRAFO 3 - Forma 3
+const parrafoTres = document.querySelector('#parrafo3');
+//REPLACECHILD
+elementoPadre.replaceChild(parrafoCuatro, parrafoTres);
