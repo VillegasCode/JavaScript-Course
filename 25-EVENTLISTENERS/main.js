f3aeae5894bd639231b28c48c6c451797a896de9
Carlos Villegas <carlos.villegasch@gmail.com>
2022-11-05 21:36:02 -0500

    btnEvento addEventListener llamando a la función capturarEvento

--- 25-EVENTLISTENERS/main.js
+++ 25-EVENTLISTENERS/main.js
@@ -1,6 +1,8 @@
-saludoBtn.addEventListener('click', ()=>{
-    console.log('Hola desde funcion flecha');
-});
+//El evento se coloca sin paréntesis porque el click lo activará
+saludoBtn.addEventListener('click', saludo);
+
+//Remover Event Listener
+//saludoBtn.removeEventListener('click', saludo);
 
 saludoBtn1.addEventListener('contextmenu', ()=>{
     console.log('Haz hecho clic derecho');
@@ -9,3 +11,14 @@ saludoBtn1.addEventListener('contextmenu', ()=>{
 saludoBtn2.addEventListener('mouseover', ()=>{
     console.log('Haz pasado el mouse encima');
 });
+
+//Botón para capturar eventos
+btnEvento.addEventListener('click', capturarEvento);
+
+function saludo() {
+    console.log("Hola desde función externa");
+}
+
+function capturarEvento(event) {
+    console.log(event);
+}
